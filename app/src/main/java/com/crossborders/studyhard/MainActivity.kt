d5a69f5dc012faf35abab47669ccc96e56f2a6d1
mustafateker <tekermustafa038@gmail.com>
2024-03-10 02:39:12 +0300

    MPAndroidChart Kütüphanesi eklendi ve performansim sayfası oluşturuldu

--- app/src/main/java/com/crossborders/studyhard/MainActivity.kt
+++ app/src/main/java/com/crossborders/studyhard/MainActivity.kt
@@ -1,104 +1,161 @@
 package com.crossborders.studyhard
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import java.text.SimpleDateFormat
 import java.util.Locale
+import android.widget.PopupMenu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.ana_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.cikis_yap){
+            auth.signOut()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+    fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        val inflater = popupMenu.menuInflater
+        inflater.inflate(R.menu.ana_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.cikis_yap -> {
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this, Login_Screen::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    startActivity(intent)
+                    finish()
+                    true // true döndürerek işlemin başarılı olduğunu belirtiyoruz
+                }
+                else -> false // tanımlanmış bir öğe değilse false döndürüyoruz
+            }
+        }
+        popupMenu.show()
+    }
+
+
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = FirebaseAuth.getInstance()
+        auth =Firebase.auth
+
 
         // Hoş geldin User_name yazdırma
-        val currentUser = auth.currentUser
-        val username = currentUser?.email?.split("@")?.get(0) ?: "Guest"
-        findViewById<TextView>(R.id.textView).apply {
-            text = "Hoş geldin $username"
-            typeface = ResourcesCompat.getFont(context, R.font.libre_franklin_semibold)
-            setTextSize(TypedValue.COMPLEX_UNIT_SP, 27.toFloat())
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            val username = user.email?.split("@")?.get(0) ?: "Guest"
+            val metin = "Hoş geldin $username"
+            val welcomeUserText: TextView = findViewById(R.id.textView)
+            welcomeUserText.text = metin
         }
 
-        val fontResourceId = R.font.montserrat_extra_bold
-        val views = arrayOf(R.id.gunTextView, R.id.saatTextView, R.id.dakikaTextView)
-        views.forEach {
-            findViewById<TextView>(it).apply {
-                typeface = ResourcesCompat.getFont(context, fontResourceId)
-                setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50.toFloat())
-            }
-        }
+        val fontResourceId = R.font.libre_franklin_semibold
+        findViewById<TextView>(R.id.textView).typeface = ResourcesCompat.getFont(this, fontResourceId)
+        findViewById<TextView>(R.id.textView).setTextSize(TypedValue.COMPLEX_UNIT_SP, 27.toFloat())
+
+        /*val playfairItalic = R.font.open_sans_regular
+        findViewById<TextView>(R.id.gunun_sozu).typeface = ResourcesCompat.getFont(this, playfairItalic)
+        findViewById<TextView>(R.id.gunun_sozu).setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25.toFloat())*/
+
+        val codepro = R.font.montserrat_extra_bold
+        findViewById<TextView>(R.id.gunTextView).typeface = ResourcesCompat.getFont(this , codepro)
+        findViewById<TextView>(R.id.gunTextView).setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50.toFloat())
+
+        findViewById<TextView>(R.id.saatTextView).typeface = ResourcesCompat.getFont(this , codepro)
+        findViewById<TextView>(R.id.saatTextView).setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50.toFloat())
+
+        findViewById<TextView>(R.id.dakikaTextView).typeface = ResourcesCompat.getFont(this , codepro)
+        findViewById<TextView>(R.id.dakikaTextView).setTextSize(TypedValue.COMPLEX_UNIT_DIP, 50.toFloat())
+
 
         // Geri sayım
-        val targetDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-            .parse("2024-06-08 10:00:00")!!.time
-        val currentTime = System.currentTimeMillis()
-        val timeDifference = targetDate - currentTime
-        object : CountDownTimer(timeDifference, 1000) {
+        val geriSayimGunText: TextView = findViewById(R.id.gunTextView)
+        val geriSayimSaatText: TextView = findViewById(R.id.saatTextView)
+        val geriSayimDakikaText: TextView = findViewById(R.id.dakikaTextView)
+
+        val hedefTarih = "2024-06-08 10:00:00"
+        val tarihFormati = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val hedefTarihZamani = tarihFormati.parse(hedefTarih)
+        val anlikTarih = System.currentTimeMillis()
+
+        val zamanFarki = hedefTarihZamani!!.time - anlikTarih
+        val start = object : CountDownTimer(zamanFarki, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                val days = millisUntilFinished / (24 * 60 * 60 * 1000)
-                val hours = (millisUntilFinished % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
-                val minutes = (millisUntilFinished % (60 * 60 * 1000)) / (60 * 1000)
+                val kalanZaman = millisUntilFinished
+                val gun = kalanZaman / (24 * 60 * 60 * 1000)
+                val saat = (kalanZaman % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+                val dakika = (kalanZaman % (60 * 60 * 1000)) / (60 * 1000)
 
-                val countdownViews = arrayOf(R.id.gunTextView, R.id.saatTextView, R.id.dakikaTextView)
-                val countdownValues = arrayOf(days, hours, minutes)
+                val geriSayimGunTextView = String.format(Locale.getDefault(), "%02d", gun)
+                val geriSayimSaatTextView = String.format(Locale.getDefault(), "%02d", saat)
+                val geriSayimDakikaTextView = String.format(Locale.getDefault(), "%02d", dakika)
 
-                countdownViews.forEachIndexed { index, viewId ->
-                    val textView = findViewById<TextView>(viewId)
-                    val value = String.format(Locale.getDefault(), "%02d", countdownValues[index])
-                    textView.text = value
-                }
+                geriSayimGunText.text = geriSayimGunTextView
+                geriSayimSaatText.text = geriSayimSaatTextView
+                geriSayimDakikaText.text = geriSayimDakikaTextView
             }
+
 
             override fun onFinish() {
-                // Implement if needed
             }
         }.start()
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.ana_menu, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.cikis_yap -> {
-                auth.signOut()
-                val intent = Intent(this, Login_Screen::class.java)
-                    .apply {
-                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                    }
-                startActivity(intent)
-                finish()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+        val denemeNetHesapla : ImageButton =findViewById(R.id.deneme_hesapla)
+        denemeNetHesapla.setOnClickListener(){
+            val denemeNetHesaplaPage = Intent(applicationContext,deneme_net_hesapla::class.java)
+            startActivity(denemeNetHesaplaPage)
         }
+        val bunu_unutma_page : ImageButton= findViewById(R.id.level)
+        bunu_unutma_page.setOnClickListener(){
+            val bunu_unutmaPage = Intent(applicationContext,bunu_unutma::class.java)
+            startActivity(bunu_unutmaPage)
+        }
+        val gecmisDenemeler : ImageButton = findViewById(R.id.gecmis_denemeler)
+        gecmisDenemeler.setOnClickListener() {
+            val gecmisDenemelerPage = Intent(applicationContext, gecmis_denemeler::class.java)
+            startActivity(gecmisDenemelerPage)
+        }
+        val performansim_Page : ImageButton = findViewById(R.id.performansim)
+        performansim_Page.setOnClickListener(){
+            Intent(applicationContext, PerformansimPage::class.java)
+        }
+
+
+
+
     }
 
-    fun onImageButtonClicked(view: View) {
-        val intent = when (view.id) {
-            R.id.deneme_hesapla -> Intent(this, deneme_net_hesapla::class.java)
-            R.id.level -> Intent(this, bunu_unutma::class.java)
-            R.id.gecmis_denemeler -> Intent(this, gecmis_denemeler::class.java)
-            R.id.performansim -> Intent(this, performansim_page::class.java)
-            else -> null
-        }
-        intent?.let { startActivity(it) }
-    }
+
 }
--- app/src/main/java/com/crossborders/studyhard/PerformansimPage.kt
+++ app/src/main/java/com/crossborders/studyhard/PerformansimPage.kt
@@ -1,8 +1,8 @@
 package com.crossborders.studyhard
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.crossborders.studyhard.databinding.ActivityPerformansimPageBinding
 import com.github.mikephil.charting.components.XAxis
@@ -12,23 +12,24 @@ import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 
-class performansim_page: AppCompatActivity() {
-    private lateinit var binding: ActivityPerformansimPageBinding
+class PerformansimPage : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPerformansimPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPerformansimPageBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+
         // Performansim başlığı font belirliyor
         val libreFranklinSemibold = R.font.libre_franklin_semibold
-        findViewById<TextView>(R.id.performansim_title).typeface =ResourcesCompat.getFont(this, libreFranklinSemibold)
+        findViewById<TextView>(R.id.performansim_title).typeface =
+            ResourcesCompat.getFont(this, libreFranklinSemibold)
 
         // Grafik oluşturma
         val barEntry = arrayListOf<BarEntry>()
-        val weeklyFollow =
-            arrayOf("Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar")
+        val weeklyFollow = arrayOf("Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar")
 
 
         barEntry.add(BarEntry(0f, 85.00f))
@@ -39,7 +40,7 @@ class performansim_page: AppCompatActivity() {
         barEntry.add(BarEntry(0f, 71.50f))
         barEntry.add(BarEntry(0f, 64.50f))
 
-        var barDataSet = BarDataSet(barEntry, "Haftalık Performans")
+        val barDataSet = BarDataSet(barEntry, "Haftalık Performans")
         barDataSet.valueTextSize = 15f
         barDataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
 
@@ -52,6 +53,5 @@ class performansim_page: AppCompatActivity() {
         binding.PerformansimBarChart.xAxis.valueFormatter = IndexAxisValueFormatter(weeklyFollow)
         binding.PerformansimBarChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
-
 
 }
