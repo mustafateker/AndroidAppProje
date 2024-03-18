8a21065fd4196340a8410932dcb205c4ce95119e
bahar demir <demirbahar628@gmail.com>
2024-03-18 15:01:49 +0300

    Tyt Deneme hesapla sayfası tasarımı ve Firebase

--- app/src/main/java/com/crossborders/studyhard/deneme_net_hesapla.kt
+++ app/src/main/java/com/crossborders/studyhard/deneme_net_hesapla.kt
@@ -1,11 +1,21 @@
 package com.crossborders.studyhard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class deneme_net_hesapla : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deneme_net_hesapla)
+
+        val TytDenemeHesapla : ImageButton = findViewById(R.id.tyt_deneme_net_hesapla_button)
+        TytDenemeHesapla.setOnClickListener(){
+            val tytDenemeHesaplaPage = Intent(applicationContext, turkce_deneme_net_hesapla::class.java)
+            startActivity(tytDenemeHesaplaPage)
+        }
     }
+
 }
--- app/src/main/java/com/crossborders/studyhard/turkce_deneme_net_hesapla.kt
+++ app/src/main/java/com/crossborders/studyhard/turkce_deneme_net_hesapla.kt
@@ -8,17 +8,26 @@ import android.text.TextWatcher
 import android.view.View // Eklenen import
 import android.widget.AdapterView // Eklenen import
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.Spinner
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
-class turkce_deneme_net_hesapla : AppCompatActivity() {
+
+
+class turkce_deneme_net_hesapla: AppCompatActivity() {
     private lateinit var turkishCorrectSpinner: Spinner
     private lateinit var turkishWrongSpinner: Spinner
-    private lateinit var turkishEmptySpinner: Spinner
-    private lateinit var turkishNetEditText: EditText
+    private lateinit var turkishemptyTextView: TextView
+    private lateinit var turkishNetTextView: TextView
+    //matemateik private
 
-    private var selectedItems: Int = 0 // Değişkenin dışarıda tanımlanması
+    private lateinit var matematikCorrectSpinner: Spinner
+    private lateinit var matematikWrongSpinner: Spinner
+    private lateinit var matematikemptyTextView: TextView
+    private lateinit var matematikNetTextView: TextView
+
+
+    private lateinit var hesaplaButton: AppCompatButton
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,40 +36,192 @@ class turkce_deneme_net_hesapla : AppCompatActivity() {
 
         turkishCorrectSpinner = findViewById(R.id.turkishCorrectSpinner)
         turkishWrongSpinner = findViewById(R.id.turkishWrongSpinner)
-        turkishEmptySpinner = findViewById(R.id.turkishEmptySpinner)
-        turkishNetEditText = findViewById(R.id.turkishNet)
+        turkishNetTextView = findViewById(R.id.turkishNet)
+        turkishemptyTextView = findViewById(R.id.turkishEmpty)
+
+        //matematik id define
+        matematikCorrectSpinner = findViewById(R.id.matematikCorrectSpinner)
+        matematikWrongSpinner = findViewById(R.id.matematikWrongSpinner)
+        matematikemptyTextView = findViewById(R.id.matematikEmpty)
+        matematikNetTextView = findViewById(R.id.matematikNet)
+
+
+        hesaplaButton = findViewById(R.id.HesaplaButton)
+
 
         initializeTurkishCorrectSpinner()
         initializeTurkishWrongSpinner()
-        initializeTurkishEmptySpinner()
-        initializeEditTexts()
-        makeSpinnersClickable()
 
-        turkishCorrectSpinner.onItemSelectedListener = createItemSelectedListener()
-        turkishWrongSpinner.onItemSelectedListener = createItemSelectedListener()
-        turkishEmptySpinner.onItemSelectedListener = createItemSelectedListener()
+        // matematik initialize
+
+
+
+        initializeTextView()
+        turkishCorrectSpinner.isClickable = true
+        turkishWrongSpinner.isClickable = true
+
+        // matematikclick
+        matematikCorrectSpinner.isClickable = true
+        matematikWrongSpinner.isClickable = true
+
+
+        turkishCorrectSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                updateEmpty()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Hiçbir şey seçilmediğinde
+            }
+        }
+
+        turkishWrongSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                updateEmpty()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+        // matematikselected
+
+        matematikCorrectSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    updateEmpty()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    // Hiçbir şey seçilmediğinde
+                }
+            }
+
+        matematikWrongSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                updateEmpty()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Hiçbir şey seçilmediğinde
+            }
+        }
+
+
+
+
+        hesaplaButton.setOnClickListener() {
+            updateNet()
+        }
     }
 
-    private fun initializeSpinner(spinner: Spinner) {
-        val items = (0..40).toList().reversed().map { it.toString() }.toTypedArray()
-        val adapter = ArrayAdapter(this, R.layout.activity_turkce_deneme_net_hesapla, items)
-        adapter.setDropDownViewResource(R.layout.activity_turkce_deneme_net_hesapla)
-        spinner.adapter = adapter
-    }
-
+    //turkce private fun/////////////////////
     private fun initializeTurkishCorrectSpinner() {
-        initializeSpinner(turkishCorrectSpinner)
+        val values = (40 downTo 0).toList().map { (40 - it).toString() }.toTypedArray()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, values)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        turkishCorrectSpinner.adapter = adapter
+
+        val adaptermatematik = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, values)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        matematikCorrectSpinner.adapter = adaptermatematik
+
+        turkishCorrectSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+        matematikCorrectSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 
     private fun initializeTurkishWrongSpinner() {
-        initializeSpinner(turkishWrongSpinner)
+        val values = (40 downTo 0).toList().map { (40 - it).toString() }.toTypedArray()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, values)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        turkishWrongSpinner.adapter = adapter
+
+        val adaptermatematik = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, values)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        matematikWrongSpinner.adapter = adaptermatematik
+
+        turkishWrongSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+        matematikWrongSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
     }
 
-    private fun initializeTurkishEmptySpinner() {
-        initializeSpinner(turkishEmptySpinner)
-    }
+    //matematik ////////////
 
-    private fun initializeEditTexts() {
+
+    private fun initializeTextView() {
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // Giriş öncesi
@@ -68,67 +229,82 @@ class turkce_deneme_net_hesapla : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 // Giriş esnasında
+
             }
 
             override fun afterTextChanged(s: Editable?) {
                 // Giriş sonrasında, neti güncelle
-                updateTurkishNet()
+
             }
         }
+        turkishemptyTextView.addTextChangedListener(textWatcher)
+        turkishNetTextView.addTextChangedListener(textWatcher)
 
-        turkishNetEditText.addTextChangedListener(textWatcher)
+        matematikemptyTextView.addTextChangedListener(textWatcher)
+        matematikNetTextView.addTextChangedListener(textWatcher)
+
     }
 
-    private fun makeSpinnersClickable() {
-        turkishCorrectSpinner.setOnTouchListener { _, _ ->
-            turkishCorrectSpinner.performClick()
-            true
-        }
-
-        turkishWrongSpinner.setOnTouchListener { _, _ ->
-            turkishWrongSpinner.performClick()
-            true
-        }
-
-        turkishEmptySpinner.setOnTouchListener { _, _ ->
-            turkishEmptySpinner.performClick()
-            true
-        }
-    }
-
-
-    private fun createItemSelectedListener(): AdapterView.OnItemSelectedListener {
-        return object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedItem = parent?.getItemAtPosition(position).toString()
-                selectedItems = selectedItems.toInt()
-                updateTurkishNet()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Hiçbir şey seçilmediğinde
-            }
-        }
-    }
-
-    private fun updateTurkishNet() {
+    private fun updateEmpty() {
         val correct = turkishCorrectSpinner.selectedItem.toString().toIntOrNull() ?: 0
         val wrong = turkishWrongSpinner.selectedItem.toString().toIntOrNull() ?: 0
-        val empty = turkishEmptySpinner.selectedItem.toString().toIntOrNull() ?: 0
+        val empty = 40 - (correct + wrong)
+        turkishemptyTextView.text = empty.toString()
 
-        var net = correct - ((wrong / 4) - empty - wrong)
-        val maxNet: Int = if (turkishCorrectSpinner.selectedItemPosition == 0) {
-            0
-        } else {
-            40
-        }
-
-        // Eğer net 40'tan büyükse veya seçilen değerlerin toplamı 40'tan büyükse hata mesajı göster
-        if (net > maxNet || correct + wrong + empty > 40) {
-            net = 0
-            Toast.makeText(this, "Hata: Net değeri maksimum değerden büyük!", Toast.LENGTH_SHORT).show()
-        }
-
-        turkishNetEditText.setText(net.toString())
+        val correctMatematik = matematikCorrectSpinner.selectedItem.toString().toIntOrNull() ?: 0
+        val wrongMatematik = matematikWrongSpinner.selectedItem.toString().toIntOrNull() ?: 0
+        val emptyMatematik = 40 - (correctMatematik + wrongMatematik)
+        matematikemptyTextView.text = emptyMatematik.toString()
     }
+
+    private fun updateNet() {
+        val correct = turkishCorrectSpinner.selectedItem.toString().toIntOrNull() ?: 0
+        val wrong = turkishWrongSpinner.selectedItem.toString().toIntOrNull() ?: 0
+
+        val correctMatematik = matematikCorrectSpinner.selectedItem.toString().toIntOrNull() ?: 0
+        val wrongMatematik = matematikWrongSpinner.selectedItem.toString().toIntOrNull() ?: 0
+
+
+
+        val net: Number = when {
+            correct == 0 && wrong == 0 -> 0 // Hiç doğru veya yanlış cevap yoksa net 0'dır
+            correct == 40 -> 40 // Tüm sorular doğru cevaplandıysa net 40'tır
+
+            correctMatematik == 0 && wrongMatematik == 0 -> 0
+            correctMatematik == 40 -> 40
+
+            else -> {
+                val wrongPenaltyTurkce = (wrong * 0.25).toFloat()
+                val totalCorrectTurkce = correct - wrongPenaltyTurkce
+                val netScoreTurkce = totalCorrectTurkce
+                netScoreTurkce
+            }
+        }
+
+        val netMatematik: Number = when {
+
+            correctMatematik == 0 && wrongMatematik == 0 -> 0
+            correctMatematik == 40 -> 40
+
+            else -> {
+                val wrongPenaltyMatematik = (wrongMatematik * 0.25).toFloat()
+                val totalCorrectMatematik = correctMatematik - wrongPenaltyMatematik
+                val netScoreMatematik = totalCorrectMatematik
+                netScoreMatematik
+            }
+        }
+
+        val currentTextMatematik = matematikNetTextView.text.toString()
+        if(currentTextMatematik != netMatematik.toString() ){
+            matematikNetTextView.setText((netMatematik.toString()))
+        }
+        val currentText = turkishNetTextView.text.toString()
+        if (currentText != net.toString()) {
+            turkishNetTextView.setText(net.toString())
+        }
+    }
+    //Matematikkkkkk privateee funn /////////////////
 }
+
+
+
